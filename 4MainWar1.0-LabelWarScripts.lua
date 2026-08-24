c81b2f8d2cfc817ba9eb3a131aed071785e8f7e6
brxyxnn7u7 <brayan.ildq@gmail.com>
2026-08-23 22:49:00 -0600

    Add files via upload

---
+++ 4MainWar1.0-LabelWarScripts.lua
@@ -0,0 +1,16 @@
+setDefaultTab("Main")
+UI.Separator()
+local botName = "WAR"
+local lNameps = UI.Label(botName)
+UI.Separator()
+
+local colors = {"red", "orange", "yellow", "green", "blue", "#00008b", "#ee82ee"}
+local colorI = 0
+macro(100, function()
+  colorI = colorI==#colors and 0 or colorI+1
+  lNameps:setColor(colors[colorI])
+end)
+
+warning = function() 
+    return  
+end
