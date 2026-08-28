1b0b1499445d6e013219c79d29046aaf117933f4
brxyxnn7u7 <brayan.ildq@gmail.com>
2026-08-28 02:05:03 -0600

    Add files via upload

---
+++ 4MainWar1.2-HoldWallbd.lua
@@ -0,0 +1,28 @@
+setDefaultTab("Main")
+storage.WGPoses = storage.WGPoses or { }
+
+macro(50, "Force Hold WG", function()
+  for _, candidate in ipairs(storage.WGPoses) do
+    local wgTile = g_map.getTile(candidate)
+    if wgTile and wgTile:canShoot() then
+      if wgTile:getTopUseThing():getId() ~= 2130 then
+        wgTile:setText("Force WG")
+        useWith(3156, wgTile:getGround())
+      end
+    end
+  end
+end)
+
+addButton("","Set WG Pos", function()
+  table.insert(storage.WGPoses, pos())
+end)
+
+addButton("","Clean All Positions", function()
+  for _, candidate in ipairs(storage.WGPoses) do
+    local wgTile = g_map.getTile(candidate)
+    if wgTile then
+      wgTile:setText("")
+    end
+  end
+  storage.WGPoses = { }
+end)
