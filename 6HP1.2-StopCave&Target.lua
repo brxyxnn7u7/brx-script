7f3e3e1d43314ccae6a237d5e63fce596572b1ba
brxyxnn7u7 <brayan.ildq@gmail.com>
2026-08-29 03:13:29 -0600

    Update 6HP1.2-StopCave&Target.lua

--- 6HP1.2-StopCave&Target.lua
+++ 6HP1.2-StopCave&Target.lua
@@ -1,24 +1,12 @@
 setDefaultTab("HP")
 
-local cIcon = addIcon("CaveBotIcon", {
-    item = {id=8154, count=1},
-    text = "Cavebot"
-}, macro(200, function(m)
+addIcon("StopCTIcon", {item={id=10227, count=1}, text="Cavebot"}, macro(200, function(m)
     CaveBot.setOn()
+    TargetBot.setOn()
+
     schedule(200, function()
         if m.isOff() then
             CaveBot.setOff()
-        end
-    end)
-end))
-
-local tIcon = addIcon("TargetBotIcon", {
-    item = {id=8160, count=1},
-    text = "Target"
-}, macro(200, function(m)
-    TargetBot.setOn()
-    schedule(200, function()
-        if m.isOff() then
             TargetBot.setOff()
         end
     end)
