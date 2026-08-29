a2c3f10547392113511a715c2cca60e9facd1057
brxyxnn7u7 <brayan.ildq@gmail.com>
2026-08-29 03:21:49 -0600

    Update 6HP1.2-StopCave&Target.lua

--- 6HP1.2-StopCave&Target.lua
+++ 6HP1.2-StopCave&Target.lua
@@ -1,15 +1,27 @@
 setDefaultTab("HP")
 
-local icon = addIcon("CaveTargetIcon", {
-    item = {id = 10227, count = 1},
-    text = "Cave\nTarget",
-    switchable = true
-}, function(widget)
-    if widget.isOn() then
-        CaveBot.setOn()
-        TargetBot.setOn()
-    else
-        CaveBot.setOff()
-        TargetBot.setOff()
-    end
+local ctIcon = addIcon("ctI",{
+  text="Cave\nTarget",
+  switchable=false,
+  moveable=true
+}, function()
+  if CaveBot.isOff() and TargetBot.isOff() then
+    CaveBot.setOn()
+    TargetBot.setOn()
+  else
+    CaveBot.setOff()
+    TargetBot.setOff()
+    g_game.attack(nil)
+  end
+end)
+
+ctIcon:setSize({height=30,width=50})
+ctIcon.text:setFont('verdana-11px-rounded')
+
+macro(50,function()
+  if CaveBot.isOn() and TargetBot.isOn() then
+    ctIcon.text:setColoredText({"Cave\nTarget\n","white","ON","green"})
+  else
+    ctIcon.text:setColoredText({"Cave\nTarget\n","white","OFF","red"})
+  end
 end)
